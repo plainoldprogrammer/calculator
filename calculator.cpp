5f94ab63a3dd96426aff3a62da3055ba3bb74a5a
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-07-08 23:27:08 -0500

    creating and showing the main window

--- calculator.cpp
+++ calculator.cpp
@@ -1,8 +1,13 @@
 #include "pch.h"
+#include "MainWindow.h"
 
 using namespace System;
+using namespace System::Windows::Forms;
 
 int main(array<System::String ^> ^args)
 {
+	calculator::MainWindow mainWindow;
+	Application::Run(%mainWindow);
+
     return 0;
 }
